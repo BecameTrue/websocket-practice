8298200a79a9f4fc0909115aab7da88901b5881e
we know <csh_do@naver.com>
2020-04-15 21:16:04 +0900

    Added some comments

--- websocket-server/index.js
+++ websocket-server/index.js
@@ -1,18 +1,23 @@
-var Express = require("express");
-var Http = require("http");
-var WebSocket = require("ws");
+const Express = require("express");
+const Http = require("http");
+const WebSocket = require("ws");
 
 const app = Express();
+
+console.log("APP\n", app);
 
 const server = Http.createServer(app);
 
 const webSocketServer = new WebSocket.Server({ server });
 
+// if message comes in to server, send same message to client.
 const onMessage = (socket, message) => {
   console.log("received : " + message);
-  socket.send("reply to : " + message);
+  socket.send("echo : " + message);
 };
 
+// if client connects, register "on message event"
+// and notify that client is connected.
 const onConnection = (socket) => {
   socket.on("message", (message) => onMessage(socket, message));
 
